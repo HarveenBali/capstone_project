0a4dd471689ebe744d8d623960cce2c9ea973b52
HarveenBali <baliharveen789@gmail.com>
2024-09-01 16:55:40 +0530

    Updated

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -105,8 +105,8 @@ const Footer: FC<HTMLAttributes<HTMLElement>> = (props): JSX.Element => {
         </Column>
       </Content>
       <Copyright>
-        <span>Developed by Abdullah Iqbal.</span>
-        <p>© 2023 Little Lemon. All rights reserved.</p>
+        <span>Developed by Harveen Kour Bali.</span>
+        <p>© 2024 Little Lemon. All rights reserved.</p>
       </Copyright>
     </Container>
   );
